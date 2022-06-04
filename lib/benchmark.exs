374c2f5f79c7d5728f633168d9052197d5a9ea12
Josh taylor <joshuataylorx@gmail.com>
2022-06-04 21:17:47 +0800

    fix benchmark

--- lib/benchmark.exs
+++ lib/benchmark.exs
@@ -1,22 +1,10 @@
-#json_data = File.read!("/Users/joshtaylor/dev/snowflake_arrow/snowflake_json.json")
-
-arrow_data =
-  File.read!(
-    "/home/josh/dev/req_snowflake/arrow/l6vz-s-aust4888results01a4b8be-3200-f010-0000-c20d00025aee0maindata061.s3_arrow"
-  )
+arrow_data_small = File.read!("benchmark/small")
+arrow_data_large = File.read!("benchmark/large")
 
 Benchee.run(
   %{
-    #    "decode" => fn -> :zlib.gunzip(arrow_data_gzipped) end,
-    #    "decode_rust" => fn -> SnowflakeArrow.Native.convert_arrow_stream(arrow_data_gzipped, false) end,
-    "naive_convert_arrow_stream" => fn ->
-      SnowflakeArrow.Native.convert_arrow_stream(arrow_data, true)
-    end
-    #    "naive_convert_arrow_stream_no_cast" => fn -> SnowflakeArrow.Native.convert_arrow_stream(arrow_data, false) end,
-    #    "naive_convert_arrow_stream_no_cast_gzipped" => fn -> SnowflakeArrow.Native.convert_arrow_stream(arrow_data_gzipped, true, true) end,
-    #    "jason_decode_snowflake_response" => fn ->
-    #      Jason.decode!("[#{json_data}]")
-    #    end,
+    "arrow_small (368kb)" => fn -> SnowflakeArrow.Native.convert_arrow_stream(arrow_data_small, true) end,
+    "arrow_large (9.4mb)" => fn -> SnowflakeArrow.Native.convert_arrow_stream(arrow_data_large, true) end,
   },
-  time: 5
+  time: 10
 )
