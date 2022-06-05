326d94dce462e8163c7e1741c8e39c3aa74e9418
Josh taylor <joshuataylorx@gmail.com>
2022-06-05 14:36:09 +0800

    rework to be a bit cleaner

--- native/snowflake_arrow/src/lib.rs
+++ native/snowflake_arrow/src/lib.rs
@@ -1,5 +1,8 @@
 mod elixir_types;
-use arrow2::array::{Array, BinaryArray, BooleanArray, Int16Array, Int32Array, Int64Array, Int8Array, PrimitiveArray, StructArray, Utf8Array};
+use arrow2::array::{
+    Array, BooleanArray, Float64Array, Int16Array, Int32Array, Int64Array, Int8Array, StructArray,
+    Utf8Array,
+};
 use arrow2::chunk::Chunk;
 use arrow2::datatypes::{DataType, Metadata};
 use arrow2::error::{ArrowError as Error, Result};
@@ -11,8 +14,19 @@ use rustler::{Encoder, Env, Term};
 use std::any::Any;
 use std::sync::Arc;
 use arrow2::io::ipc::read::StreamMetadata;
-use crate::ArrowType::{ABinaryArray, ABooleanArray, AF64Array, AFloat64Array, AInt16Array, AInt32Array, AInt64Array, AInt8Array, AMissing, AStringVec, AUtf8Array};
-use crate::atoms::nil;
+
+// #[derive(Serialize)]
+pub enum ReturnType<'a> {
+    Int32(Vec<Option<&'a i32>>),
+    Int64(Vec<Option<&'a i64>>),
+    Float64(Vec<Option<&'a f64>>),
+    Int16(Vec<Option<&'a i16>>),
+    Int8(Vec<Option<&'a i8>>),
+    Utf8(Vec<Option<&'a str>>),
+    Boolean(Vec<Option<bool>>),
+    String(Vec<Option<String>>),
+    Missing,
+}
 
 mod atoms {
     rustler::atoms! {
@@ -24,141 +38,125 @@ mod atoms {
 }
 
 rustler::init!("Elixir.SnowflakeArrow.Native", [convert_arrow_stream]);
-pub enum ArrowType<'a> {
-    AInt64Array(Option<&'a PrimitiveArray<i64>>),
-    AInt32Array(Option<&'a PrimitiveArray<i32>>),
-    AInt16Array(Option<&'a PrimitiveArray<i16>>),
-    AInt8Array(Option<&'a PrimitiveArray<i8>>),
-    ABooleanArray(Option<&'a BooleanArray>),
-    AUtf8Array(Option<&'a Utf8Array<i32>>),
-    AStringVec(Option<Vec<Option<String>>>),
-    // ADateVec(Option<Vec<Option<ElixirDate>>>),
-    ABinaryArray(Option<&'a BinaryArray<i32>>),
-    AFloat64Array(Option<&'a PrimitiveArray<f64>>),
-    AMissing(String),
-    AF64Array(Option<Vec<Option<f64>>>)
-}
+// use rayon::prelude::*;
 
 fn metadata_for_field(metadata: StreamMetadata, index: u32) -> Metadata {
-    return metadata.schema.fields.get(index as usize).unwrap().metadata.clone();
+    return metadata
+        .schema
+        .fields
+        .get(index as usize)
+        .unwrap()
+        .metadata
+        .clone();
 }
 
 #[rustler::nif]
 pub fn convert_arrow_stream<'a>(
     env: Env<'a>,
     arrow_stream_data: Binary,
-    cast_elixir_types: bool
+    _cast_elixir_types: bool,
 ) -> Term<'a> {
     let (metadata, chunk) = convert_binary(arrow_stream_data).unwrap();
-    let mut vec: Vec<ArrowType> = Vec::new();
+    chunk
+        .iter()
+        .enumerate()
+        .map(|(field_index, field)| {
+            let field_metadata = metadata_for_field(metadata.clone(), field_index as u32);
 
-    let mut i = 0;
-    for field in chunk.arrays() {
-        // get column metadata
-        let field_metadata = metadata_for_field(metadata.clone(), i);
-
-        let column = field.as_any();
-        match field.data_type() {
-            DataType::Int64 => vec.push(AInt64Array(column.downcast_ref::<Int64Array>())),
-            DataType::Int32 => {
-                if field_metadata.get("scale").unwrap() == "0" {
-                    vec.push(AInt32Array(column.downcast_ref::<Int32Array>()));
-                } else {
-                    let scale = field_metadata.get("scale").unwrap().parse::<i32>().unwrap();
-                    let dc = column.downcast_ref::<Int32Array>().unwrap();
-                    let mut float_vecs: Vec<Option<f64>> = Vec::new();
-
-                    let mut float_index = 0;
-                    for valid in dc {
-                        if dc.is_null(float_index) {
-                            float_vecs.push(None);
-                        } else {
-                            let f = (*valid.unwrap() as f64) / (10.0f64.powi(scale as i32));
-                            float_vecs.push(Some(f));
+            let column = field.as_any();
+            match field.data_type() {
+                DataType::Int64 => ReturnType::Int64(
+                    column
+                        .downcast_ref::<Int64Array>()
+                        .unwrap()
+                        .iter()
+                        .collect::<Vec<_>>(),
+                ),
+                // // .encode(env),
+                DataType::Int32 => ReturnType::Int32(
+                    column
+                        .downcast_ref::<Int32Array>()
+                        .unwrap()
+                        .iter()
+                        .collect::<Vec<_>>(),
+                ),
+                DataType::Float64 => ReturnType::Float64(
+                    column
+                        .downcast_ref::<Float64Array>()
+                        .unwrap()
+                        .iter()
+                        .collect::<Vec<_>>(),
+                ),
+                // .encode(env),
+                DataType::Int16 => ReturnType::Int16(
+                    column
+                        .downcast_ref::<Int16Array>()
+                        .unwrap()
+                        .iter()
+                        .collect::<Vec<_>>(),
+                ),
+                DataType::Int8 => ReturnType::Int8(
+                    column
+                        .downcast_ref::<Int8Array>()
+                        .unwrap()
+                        .iter()
+                        .collect::<Vec<_>>(),
+                ),
+                // // .encode(env),
+                DataType::Utf8 => ReturnType::Utf8(
+                    column
+                        .downcast_ref::<Utf8Array<i32>>()
+                        .unwrap()
+                        .iter()
+                        .collect::<Vec<_>>(),
+                ),
+                // // .encode(env),
+                DataType::Date32 =>
+                // Not sure if this is the best way to convert into NaiveDates
+                {
+                    ReturnType::String(
+                        column
+                            .downcast_ref::<Int32Array>()
+                            .unwrap()
+                            .iter()
+                            .map(|t| match t {
+                                Some(v) => Some(date32_to_date(*v).to_string()),
+                                None => None,
+                            })
+                            .collect::<Vec<Option<String>>>(),
+                    )
+                }
+                DataType::Boolean => ReturnType::Boolean(
+                    column
+                        .downcast_ref::<BooleanArray>()
+                        .unwrap()
+                        .iter()
+                        .collect::<Vec<_>>(),
+                ),
+                DataType::Struct(_f) => {
+                    let logical_type = field_metadata.get("logicalType").unwrap().as_str();
+                    match logical_type {
+                        "TIMESTAMP_NTZ" | "TIMESTAMP_LTZ" => {
+                            return ReturnType::String(convert_timestamps(column));
                         }
-                        float_index += 1;
+                        _ => ReturnType::Missing,
                     }
-
-                    vec.push(AF64Array(Some(float_vecs)));
                 }
-            },
-            DataType::Int16 => vec.push(AInt16Array(column.downcast_ref::<Int16Array>())),
-            DataType::Int8 => vec.push(AInt8Array(column.downcast_ref::<Int8Array>())),
-            DataType::Boolean => vec.push(ABooleanArray(column.downcast_ref::<BooleanArray>())),
-            DataType::Utf8 => { vec.push(AUtf8Array(column.downcast_ref::<Utf8Array<i32>>())) },
-            DataType::Float64 => vec.push(AFloat64Array(column.downcast_ref::<PrimitiveArray<f64>>())),
-            DataType::Date32 => {
-                if cast_elixir_types {
-                    // let dates = convert_dates(column);
-                    // vec.push(ADateVec(Some(dates)));
-                } else {
-                    let mut date_vec: Vec<Option<String>> = Vec::new();
-                    for date in column.downcast_ref::<Int32Array>().unwrap() {
-                        match date {
-                            Some(d) => {
-                                let value = date32_to_date(*d as i32).to_string();
-                                date_vec.push(Some(value));
-                            },
-                            None => date_vec.push(None)
-                        }
-                    }
-                    vec.push(AStringVec(Some(date_vec)));
-                }
-            },
-            DataType::Struct(_f) => {
-                let logical_type = field_metadata.get("logicalType").unwrap().as_str();
-                match logical_type {
-                    "TIMESTAMP_NTZ" | "TIMESTAMP_LTZ" => {
-                        let date_vec = convert_timestamps(column);
-                        vec.push(AStringVec(Some(date_vec)));
-                    }
-                    _ => {}
-                }
-            },
-            DataType::Binary => { vec.push(ABinaryArray(column.downcast_ref::<BinaryArray<i32>>())) },
-            data_type => {
-                vec.push(AMissing(format!("{:?}", data_type)));
+                _ => ReturnType::Missing, //field.data_type().to_string(),
             }
-            // When using local branch, I added formatting to arrow2 to show the datatype as a string,
-            // so I knew the types that weren't working yet
-            // vec.push(to_term(env, data_type.to_string()).unwrap()),
-        }
-
-        i+=1;
-    }
-
-    return vec.encode(env);
+        })
+        .collect::<Vec<ReturnType>>()
+        .encode(env)
 }
-
-// Function to convert dates to Elixir dates.
-// fn convert_dates(column: &dyn Any) -> Vec<Option<ElixirDate>> {
-//     let dates_array = column.downcast_ref::<Int32Array>().unwrap();
-//
-//     let mut date_vec: Vec<Option<ElixirDate>> = Vec::new();
-//     for date in dates_array {
-//         match date {
-//             Some(d) => {
-//                 let date = date32_to_date(*d as i32);
-//                 // Code to convert to an ElixirDate
-//                 let date = ElixirDate {
-//                     year: date.year(),
-//                     month: date.month(),
-//                     day: date.day(),
-//                 };
-//                 date_vec.push(Some(date));
-//             }
-//             None => {
-//                 date_vec.push(None);
-//             }
-//         }
-//     }
-//     return date_vec;
-// }
 
 fn convert_timestamps(column: &dyn Any) -> Vec<Option<String>> {
     let mut date_vec: Vec<Option<String>> = Vec::new();
 
-    let (_fields, arrays, _bitmap) = column.downcast_ref::<StructArray>().unwrap().clone().into_data();
+    let (_fields, arrays, _bitmap) = column
+        .downcast_ref::<StructArray>()
+        .unwrap()
+        .clone()
+        .into_data();
 
     // get the epochs from the first array
     let epochs = arrays[0].as_any().downcast_ref::<Int64Array>().unwrap();
@@ -172,7 +170,9 @@ fn convert_timestamps(column: &dyn Any) -> Vec<Option<String>> {
             Some(epoch) => {
                 let value1 = *epoch as i64;
                 let value2 = fractions.value(i);
-                date_vec.push(Some(NaiveDateTime::from_timestamp(value1, value2 as u32).to_string()));
+                date_vec.push(Some(
+                    NaiveDateTime::from_timestamp(value1, value2 as u32).to_string(),
+                ));
             }
             None => {
                 date_vec.push(None);
@@ -183,7 +183,9 @@ fn convert_timestamps(column: &dyn Any) -> Vec<Option<String>> {
 }
 
 // Converts a Elixir binary to Chunk<Arc<dyn Array>> using arrow2
-pub fn convert_binary(arrow_stream_data: Binary) -> Result<(StreamMetadata, Chunk<Arc<dyn Array>>)> {
+pub fn convert_binary(
+    arrow_stream_data: Binary,
+) -> Result<(StreamMetadata, Chunk<Arc<dyn Array>>)> {
     let mut binary = arrow_stream_data.as_ref();
 
     // We want the metadata later for checking types
@@ -209,25 +211,18 @@ pub fn convert_binary(arrow_stream_data: Binary) -> Result<(StreamMetadata, Chun
     ));
 }
 
-impl<'b> Encoder for ArrowType<'b> {
+impl<'b> Encoder for ReturnType<'b> {
     fn encode<'a>(&self, env: Env<'a>) -> Term<'a> {
-        return match self {
-            AFloat64Array(dt) => dt.unwrap().iter().collect::<Vec<_>>().encode(env),
-            AInt32Array(dt) => dt.unwrap().iter().collect::<Vec<_>>().encode(env),
-            AInt64Array(dt) => dt.unwrap().iter().collect::<Vec<_>>().encode(env),
-            AInt16Array(dt) => dt.unwrap().iter().collect::<Vec<_>>().encode(env),
-            AInt8Array(dt) => dt.unwrap().iter().collect::<Vec<_>>().encode(env),
-            ABooleanArray(dt) => dt.unwrap().iter().collect::<Vec<_>>().encode(env),
-            AUtf8Array(dt) => dt.unwrap().iter().collect::<Vec<_>>().encode(env),
-            ABinaryArray(dt) => dt.unwrap().iter().collect::<Vec<_>>().encode(env),
-            AStringVec(dt) => {
-                match dt {
-                    Some(x) => x.iter().collect::<Vec<_>>().encode(env),
-                    None => nil().encode(env),
-                }
-            }
-            AF64Array(dt) => dt.iter().collect::<Vec<_>>().encode(env),
-            AMissing(missing) => missing.encode(env)
-        };
+        match self {
+            ReturnType::Int32(a) => a.encode(env),
+            ReturnType::Int64(a) => a.encode(env),
+            ReturnType::Float64(a) => a.encode(env),
+            ReturnType::Int16(a) => a.encode(env),
+            ReturnType::Int8(a) => a.encode(env),
+            ReturnType::Utf8(a) => a.encode(env),
+            ReturnType::String(a) => a.encode(env),
+            ReturnType::Boolean(a) => a.encode(env),
+            ReturnType::Missing => "missing".encode(env),
+        }
     }
 }
---
+++ test/varchar_conversion_test.exs
@@ -0,0 +1,21 @@
+defmodule SnowflakeArrow.TextConversionTest do
+  use ExUnit.Case, async: true
+  alias SnowflakeArrow.Native
+
+  test "Can convert text to string without elixir types" do
+    data =
+      File.read!(
+        Path.join([
+          :code.priv_dir(:snowflake_arrow),
+          "testing/base64/SF_VARCHAR.arrow"
+        ])
+      )
+      |> Base.decode64!()
+
+    values = Native.convert_arrow_stream(data, false)
+
+    assert values == [
+             [nil, nil, "fF7Lk7YKfXtYFsY2fVXN", nil, "dvhJoe9A0qtgRJwHFMHN", nil, nil, "dBmT57wW3U0pLaJp9PGV", nil, "XRFjUy5wdbzE7k1z9SSa"]
+           ]
+  end
+end
