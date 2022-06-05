655010119b56062200f469406b8766349dd51919
Josh Taylor <joshuataylorx@gmail.com>
2022-06-05 14:54:17 +0800

    benchmark data for json/arrow

--- lib/benchmark.exs
+++ lib/benchmark.exs
@@ -1,10 +1,14 @@
-arrow_data_small = File.read!("benchmark/small")
-arrow_data_large = File.read!("benchmark/large")
+arrow_data_small = File.read!("benchmark/small_arrow")
+arrow_data_large = File.read!("benchmark/large_arrow")
+jason_data_large = File.read!("benchmark/large_json.json")
+jason_data_small = File.read!("benchmark/small_json.json")
 
 Benchee.run(
   %{
     "arrow_small (368kb)" => fn -> SnowflakeArrow.Native.convert_arrow_stream(arrow_data_small, true) end,
     "arrow_large (9.4mb)" => fn -> SnowflakeArrow.Native.convert_arrow_stream(arrow_data_large, true) end,
+    "jason_decode (8.8mb)" => fn -> SnowflakeArrow.Native.convert_arrow_stream(jason_data_large, true) end,
+    "jason_decode (468Kb)" => fn -> SnowflakeArrow.Native.convert_arrow_stream(jason_data_small, true) end,
   },
-  time: 10
+  time: 5
 )
